4bc96e7b7ff3555e3962fb540382fdf52921bd41
GiaHuy-251 <lngiahuy2007@gmail.com>
2026-01-29 07:55:12 +0700

    Add C program of <110125228_LeNguyenGiaHuy>

---
+++ 110125228.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b,tong,hieu,tich;
+	float thuong;
+	
+	printf("nhap so nguyen a: ");
+	scanf("%d",&a);
+	printf("\nnhap so nguyen b: ");
+	scanf("%d",&b);
+	
+	tong=a+b;
+	hieu=a-b;
+	tich=a*b;
+	thuong=1.0*a/b;
+	
+	printf("\ntong 2 so nguyen la: %d",tong);
+	printf("\nhieu 2 so nguyen la: %d",hieu);
+	printf("\ntich 2 so nguyen la: %d",tich);
+	printf("\nthuong 2 so nguyen la: %3.2f",thuong);
+	
+	return 0;
+}
